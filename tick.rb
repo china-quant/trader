f7f2bcf66ee589c4c889874b3d203bdb710a55f7
Tenari <djz24@case.edu>
2014-09-18 14:15:37 -0500

    re-created Mole's TICK indicator for trend days

---
+++ tick.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+
+samples = 2600
+
+def getTICK
+  Net::HTTP.start('www.barchart.com') do |http|
+    resp = http.post("/data/json-quotes.phpx", 
+    "keys=%24TICK&extrafields=last_t", {
+      "Content-Type" => "application/x-www-form-urlencoded",
+      "Referer" => "http://www.barchart.com/quotes/stocks/$TICK"
+    })
+    return resp.body.slice(12,9).gsub(",","").to_i
+  end
+end
+
+ticks = []
+
+samples.times do |i|
+  sleep 1
+  ticks.push getTICK
+end
+
+pos = 0
+neg = 0
+ticks.each do |tick|
+ pos += 1 if tick > 10000
+ neg += 1 if tick < 10000
+end
+
+puts ticks
+
+if pos/samples > 0.9
+  puts "HIGH probability of UP-trend day"
+elsif pos/samples > 0.8
+  puts "MODERATE probability of UP-trend day"
+else
+  puts "probably not trending up"
+end
+if neg/samples > 0.9
+  puts "HIGH probability of DOWN-trend day"
+elsif neg/samples > 0.8
+  puts "MODERATE probability of DOWN-trend day"
+else
+  puts "probably not trending down"
+end
+
